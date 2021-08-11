f00ed3b159aa9d23da1bf0824e0efc901b4f8885
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2021-08-11 15:11:05 +0200

    Modify sed command to only switch the first-ocurrence

--- sh/extract_url.sh
+++ sh/extract_url.sh
@@ -17,7 +17,7 @@ echo "$EA_REPO_YAML"
 
 #Substitue the entire line where a match to $EA_REPO has been found thanks to .* after the matching 
 #string. If .* is not present, only the matching string gets substitued.
-sed -i "s|$EA_REPO.*|$EA_REPO_YAML|" $YAML_FILE
+sed -i "0,/$EA_REPO/{s|$EA_REPO.*|$EA_REPO_YAML|}" $YAML_FILE
 
 #Do the same process (almost) for the SHA line
 #1 obtain the right line
@@ -27,7 +27,7 @@ echo "$EA_SHA_TMP"
 
 #switch the lines
 
-sed -i "s|$EA_SHA.*|$EA_SHA_TMP|" $YAML_FILE
+sed -i "0,/$EA_SHA/{s|$EA_SHA.*|$EA_SHA_TMP|}" $YAML_FILE
 
 
 
