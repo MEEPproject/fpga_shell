9b3260a4810e3192531241f42f4132934ce82397
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2021-11-10 03:43:10 +0100

    Add acc path variable

--- tcl/environment.tcl
+++ tcl/environment.tcl
@@ -6,6 +6,7 @@ set g_current_vivado_version [version -short]
 set g_project_name system      
 set g_root_dir    [pwd]                     
 set g_project_dir ${g_root_dir}/project    
+set g_accel_dir ${g_root_dir}/accelerator
 set g_design_name ${g_project_name}       
 set g_top_module  ${g_root_dir}/src/${g_project_name}_top.vhd
 set g_useBlockDesign Y 	  
