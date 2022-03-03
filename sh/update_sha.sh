b918a7fa04c89d808645fa86538446ee97858082
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-03-03 11:26:22 +0100

    Add the License header to all relevant files. Add apache LICENSE in the root path. The IPs on the submodules should also have the same headers

--- sh/update_sha.sh
+++ sh/update_sha.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# Copyright 2022 Barcelona Supercomputing Center-Centro Nacional de Supercomputación
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Daniel J.Mazure, BSC-CNS
+# Date: 22.02.2022
+# Description: 
+
+
+#!/bin/bash
 
 ROOT_DIR=$(pwd)
 EA_SHA="EMULATED_ACCELERATOR_SHA"
