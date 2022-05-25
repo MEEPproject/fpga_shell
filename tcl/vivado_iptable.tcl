102c7b836ef7d788ddb6259beea341bccda07f38
Daniel J. Mazure <dasjimaz@gmail.com>
2022-05-26 01:33:47 +0200

    Update vivado ip table to support Vivado 2022.1

--- tcl/vivado_iptable.tcl
+++ tcl/vivado_iptable.tcl
@@ -26,6 +26,9 @@ switch $g_vivado_version {
 	2021.2 {
 		set meep_util_ds_buf "xilinx.com:ip:util_ds_buf:2.2"
 	}
+	2022.1 {
+                set meep_util_ds_buf "xilinx.com:ip:util_ds_buf:2.2"
+	}
 }
 
 switch $g_board_part {
