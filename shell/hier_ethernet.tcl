9eed1301c30a16a44f4cab243b6f3dd8e55cba13
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-09-29 23:02:56 +0200

    Fix reset connections inside the ethernet hierarchy

--- shell/hier_ethernet.tcl
+++ shell/hier_ethernet.tcl
@@ -106,8 +106,9 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 xlconcat_irq
   connect_bd_net -net ethernet_alveo_0_eth_gt_user_clock [get_bd_pins eth_gt_user_clock] [get_bd_pins ${EthIPName}/eth_gt_user_clock] [get_bd_pins axi_dma_0/m_axi_mm2s_aclk] [get_bd_pins axi_dma_0/m_axi_s2mm_aclk] [get_bd_pins axi_dma_0/m_axi_sg_aclk] [get_bd_pins axi_interconnect_0/ACLK] [get_bd_pins axi_interconnect_0/S00_ACLK] [get_bd_pins axi_interconnect_0/S01_ACLK] [get_bd_pins axi_interconnect_0/S02_ACLK] [get_bd_pins proc_sys_reset_eth/slowest_sync_clk] [get_bd_pins axi_dma_0/s_axi_lite_aclk] [get_bd_pins axi_interconnect_0/M00_ACLK] [get_bd_pins smartconnect_0/aclk]
 
   connect_bd_net -net proc_sys_reset_eth_interconnect_aresetn [get_bd_pins axi_interconnect_0/ARESETN] [get_bd_pins proc_sys_reset_eth/interconnect_aresetn]
-  connect_bd_net -net proc_sys_reset_eth_peripheral_aresetn [get_bd_pins axi_interconnect_0/S00_ARESETN] [get_bd_pins axi_interconnect_0/S01_ARESETN] [get_bd_pins axi_interconnect_0/S02_ARESETN] [get_bd_pins axi_dma_0/axi_resetn] [get_bd_pins axi_interconnect_0/M00_ARESETN] [get_bd_pins smartconnect_0/aresetn] [get_bd_pins ${EthIPName}/eth_gt_resetn] 
-connect_bd_net [get_bd_pins eth_ext_rstn] [get_bd_pins proc_sys_reset_eth/ext_reset_in]
+  connect_bd_net -net proc_sys_reset_eth_peripheral_aresetn [get_bd_pins proc_sys_reset_eth/peripheral_aresetn] [get_bd_pins axi_interconnect_0/S00_ARESETN] [get_bd_pins axi_interconnect_0/S01_ARESETN] [get_bd_pins axi_interconnect_0/S02_ARESETN] [get_bd_pins axi_dma_0/axi_resetn] [get_bd_pins axi_interconnect_0/M00_ARESETN] [get_bd_pins smartconnect_0/aresetn] 
+  connect_bd_net [get_bd_pins eth_gt_rstn] [get_bd_pins proc_sys_reset_eth/peripheral_aresetn]
+  connect_bd_net [get_bd_pins eth_ext_rstn] [get_bd_pins proc_sys_reset_eth/ext_reset_in]
 
 
   connect_bd_net [get_bd_pins axi_dma_0/mm2s_introut] [get_bd_pins xlconcat_irq/In0]
