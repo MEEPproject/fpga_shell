c980f861f82f7a7cb68faf9cefcbb1440c3582b5
bsabater <blanca.sabater@bsc.es>
2023-02-16 18:53:36 +0100

    hardcoded element fixed

--- misc/read.sh
+++ misc/read.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-
 ADDR=$1
-PCIE_SLOT=lspci -m -d 10ee:| cut -d " " -f 1 | cut -d ":" -f 1
+PCIE_SLOT=`lspci -m -d 10ee:| cut -d " " -f 1 | cut -d ":" -f 1`
 #We are filtering the output of lscpi (list of PCIe in the system) with the vendor ID (Xilinx PCIe) 10ee, and the flag -d
 #this number will differ depending on the server, so it ought not be hardcoded. 
 #result of this will be a 2 digit number z.B. 08
+
 QDMA_PCI="qdma${PCIE_SLOT}000"
 
 echo $QDMA_PCI
 
-
-#echo -e "READ 4 bytes from ADDRESS $ADDR\r\n"
-
-dma-ctl qdma09000 reg write bar 2 0x0 0x3 >> null
+dma-ctl $QDMA_PCI reg write bar 2 0x0 0x3 >> null
 
 sleep 0.1
 
