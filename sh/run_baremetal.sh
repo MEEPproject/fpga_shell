1c3a6e498768a9ca3830adda2803ddbf9d43384f
Francelly Cano <francelly.canoladino@bsc.es>
2023-03-14 15:49:59 +0100

    #TestCICD. Fixed the way to boot the baremetal binaries and get the output at the same time

--- sh/run_baremetal.sh
+++ sh/run_baremetal.sh
@@ -86,6 +86,66 @@ function set_file() {
    sed -i -e 's#^#./tmp/bin/#'  $1
 
 }
+
+function test_loop_log() {
+TEST_LIST=$1
+OUTPUT_FILE=$2
+
+readarray -t test_riscv < $TEST_LIST
+
+#we open pico to print output results
+#n this script, the & character at the end of the picocom command runs it in the background, so the script can continue executing while picocom is running.
+
+stty -F /dev/ttyUSB2 115200 inlcr -echo
+cat < /dev/ttyUSB2 > $OUTPUT_FILE &
+#nohup sh -c "cat /dev/ttyUSB2 > $OUTPUT_FILE" &
+
+start_time=$(date +%s.%N)   # get start time in seconds.nanoseconds
+
+for i in "${test_riscv[@]}"
+do
+   #echo "$i"
+   source $BOOT_FILE/boot_acme.sh $i
+   echo -e " The result of  ${Y}$i ${NC} test was printed "
+   tail $OUTPUT_FILE
+   sleep 20
+   
+done
+
+end_time=$(date +%s.%N)     # get end time in seconds.nanoseconds
+elapsed_time=$(echo "$end_time - $start_time" | bc)   # calculate elapsed time
+
+echo "Elapsed time: $elapsed_time seconds"
+
+echo "Kill cat process ..."
+
+{ 
+	sudo pkill cat 2> $OUTPUT_FILE.error 
+} || { 
+	echo "pkill cat command failed:"
+	cat $OUTPUT_FILE.error || ls
+}
+
+#Add a carriage return
+echo -e "\r\n" >> $OUTPUT_FILE
+
+echo "FPGA Test completed."
+echo "pico status:"
+ps -ef | grep ttyUSB2
+
+}
+
+function pico_close() {
+
+   # Send Ctrl-A
+   echo -ne '\x01'
+
+   # Send Ctrl-X
+   echo -ne '\x18'
+
+   # Wait for picocom to exit
+  # wait
+}
 #menu
 if [ $1 == setup ]; then
    setup
@@ -93,6 +153,10 @@ elif [ $1 == test_loop ]; then
    run_loop_test $2
 elif [ $1 == set_file ]; then
    set_file $2
+elif [ $1 == test_loop_log ]; then
+   test_loop_log $2 $3
+elif [ $1 == pico_close ]; then
+   pico_close 
 elif [ $1 == clean_log ]; then
    clean_log $2 $3
 fi
