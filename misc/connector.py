9dc676975982fde5b53dcc5cb5e6a5a7ccf9db80
Daniel J.Mazure <dasjimaz@gmail.com>
2023-04-12 23:18:56 +0000

    Develop

--- misc/connector.py
+++ misc/connector.py
@@ -22,43 +22,59 @@ print("Connection established to: ",data)
 # Random number above 2
 numline = 3
 
+# Parse the file. It looks for the line where the resources are shown from the "top" point of view
 with open('utilization_hier.rpt', 'r') as file:
     line = file.readline()
     
     for line in file:
-        #print(line.rstrip())
+        # Regular expresion to search for the instance word. It marks the beginning of the relevant data
         m = re.search(r"Instance.*", line.rstrip())
         numline += 1
 
+        # When there is a match, get the whole line.
         if m:
+            # Grab the whole line
             m.group()
+            # Cast to string
             m=str(m.group())
+            # Remove spaces
             m = " ".join(m.split())
             resources = ''.join(m)
             
             print(resources)       
             # Reset counter
             numline = 0
+        # Two lines after, there is the actual data we are looking for. 
         if numline == 2:
+            # Remove the spaces at the end of the line
             data = line.rstrip()
+            # Remove all the remaining spaces
             data = " ".join(data.split())
+            # Concatena into a single string
             data = ''.join(data)
             print(data)
 
 resources = resources.split('|')
 values = data.split('|')
 
+# Debuggins, shows data
+print("olakase")
+del values[0:3]
 for amount in values:
     print(amount)
 
+# Remove the spaces
+print("olakase1")
+del resources[0:2]
 for field in resources:
-    field = field.replace(" ","")    
+    field = field.replace(" ","")  
     print(field)
 
 
 #Dropping EMPLOYEE table if already exists.
 cursor.execute("DROP TABLE IF EXISTS FPGA_RESOURCES")
 
+# Creates the table creatin template
 sql_table ='''CREATE TABLE FPGA_RESOURCES(
    BITSTREAM_ID CHAR(20) NOT NULL,
    NAME CHAR(20) NOT NULL,
@@ -66,10 +82,12 @@ sql_table ='''CREATE TABLE FPGA_RESOURCES(
 
 ins_values = ""
 
+# Concatenates the resources than have been collected by Vivado
 for field in resources:
     field = field.replace(" ","")
+    # This if discards empty values
     if field:
-        sql_table  += ",\n   {} CHAR(20)".format(field)
+        sql_table  += ",\n   {} INT".format(field)
         ins_values += ", {}".format(field)
 
 sql_table  += "\n   )"
@@ -135,11 +153,4 @@ result = cursor.fetchall();
 print(result)
 
 
-
-#Retrieving the list of databases
-#print("List of databases: ")
-#cursor.execute("SHOW DATABASES;")
-#print(cursor.fetchall())
-
-#Closing the connection
 conn.close()
