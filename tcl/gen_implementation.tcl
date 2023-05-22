7f72022b1428ec722a972544a91b4e3dfeaa80e4
Alex Kropotov <alexander.kropotov@bsc.es>
2023-05-22 09:00:59 +0200

    #TestCICD Returning Auto_2 placement strategy as better for heavy designs for U55C, changing route strategy to NoTimingRelaxation.

--- tcl/gen_implementation.tcl
+++ tcl/gen_implementation.tcl
@@ -288,14 +288,14 @@ set directivesFile $g_root_dir/shell/directives.tcl
 set g_place_directive "ExtraNetDelay_low"
 set g_place_directive "Explore"
 set g_place_directive "ExtraTimingOpt"
-set g_place_directive "Auto_3"
+set g_place_directive "Auto_2"
 
-set g_route_directive "NoTimingRelaxation"
 set g_route_directive "AggressiveExplore"
+set g_route_directive "NoTimingRelaxation"
 
 if { $g_board_part == "u280" }  {
   # board-specific strategies in case it helps for heavy designs
-  set g_place_directive "Auto_3"
+  set g_place_directive "Explore"
   set g_route_directive "AggressiveExplore"
 }
 
